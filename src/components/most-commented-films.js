99125be52783ae0cafd745ba00cf6ef27fb23b3a
Aleksei Amosov <amos8moses@gmail.com>
2019-11-24 20:33:30 +0300

    split main.js into components and install webpack and npm

---
+++ src/components/most-commented-films.js
@@ -0,0 +1,12 @@
+import { createFilmCardTemplate } from "./film-card";
+
+export const mostCommentedFilmTemplate = () => {
+  return `<section class="films-list--extra">
+      <h2 class="films-list__title">Most commented</h2>
+  
+      <div class="films-list__container">
+      ${createFilmCardTemplate()}
+      ${createFilmCardTemplate()}
+      </div>
+    </section>`;
+};
